a5fa1320c655d487da372c7c3675136754b416a1
draco <122390064+Darco786@users.noreply.github.com>
2023-02-24 19:15:21 +0530

    Update NftProfile.js

--- src/components/NftProfile.js
+++ src/components/NftProfile.js
@@ -1,46 +1,53 @@
-import React from 'react'
-import NftPro from '../assets/nft-pro.png'
-import Avatar from '../assets/avatar.png'
-import {IoHeartCircle} from 'react-icons/io5'
+import React from "react";
+import NftPro from "../assets/nft-pro.png";
+import Avatar from "../assets/avatar.png";
+import { IoHeartCircle } from "react-icons/io5";
 function NftProfile() {
   return (
     <section>
-        <div className='container'>
-            <div className='flex main-box'>
-                <div className=''>
-                    <img src={NftPro} alt=""/>
-                </div>
-                <div>
-                    <div>   <h1>BTSEA PUNKS</h1>
-                    <p>#2075</p></div>
-                 
-                    <p>Lorem ipsum dolor sit amet consectetur. Risus ut id velit hendrerit nunc tristique non malesuada malesuada. Eu lacus sed imperdiet elementum ipsum duis at ridiculus rhoncus. Lorem ipsum dolor sit amet consectetur. Risus ut id velit hendrerit nunc tristique non malesuada malesuada. Eu lacus sed imperdiet elementum ipsum duis at ridiculus rhoncus. </p>
-                
-                <div>
-                    <div className='flex'>
-                        <div> 
-                             <img src={Avatar} alt=""/>
-                        <div>
-                            <p>Owned by</p>
-                            <span>Amirhossein Rahmani</span>
-                        </div>
-                        </div>
-                        <div>
-                            <a href="/">0.80 BTC</a>
-                            <span><IoHeartCircle/></span>
-                        </div>
-                      
-                    </div>
-                </div>
-                </div>
-
+      <div className="container">
+        <div className="flex main-box">
+          <div className="">
+            <img src={NftPro} alt="" />
+          </div>
+          <div>
+            <div>
+              {" "}
+              <h1>BTSEA PUNKS</h1>
+              <p>#2075</p>
             </div>
 
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Risus ut id velit
+              hendrerit nunc tristique non malesuada malesuada. Eu lacus sed
+              imperdiet elementum ipsum duis at ridiculus rhoncus. Lorem ipsum
+              dolor sit amet consectetur. Risus ut id velit hendrerit nunc
+              tristique non malesuada malesuada. Eu lacus sed imperdiet
+              elementum ipsum duis at ridiculus rhoncus.{" "}
+            </p>
+
+            <div>
+              <div className="flex">
+                <div>
+                  <img src={Avatar} alt="" />
+                  <div>
+                    <p>Owned by</p>
+                    <span>Amirhossein Rahmani</span>
+                  </div>
+                </div>
+                <div>
+                  <a href="/">0.80 BTC</a>
+                  <span>
+                    <IoHeartCircle />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-
+      </div>
     </section>
-  )
+  );
 }
 
-export default NftProfile
+export default NftProfile;
